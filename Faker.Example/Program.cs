6de1c3322266d1931975404698e7a292780b32fd
watermelonrytp@gmail.com <Felix>
2022-09-16 01:51:21 +0300

    remove old code

--- Faker.Example/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Faker.Core.Interfaces;
-using Faker.Core;
-
-IFaker faker = new Faker1();
-
-Console.WriteLine(faker.Create<uint>().ToString());
-Console.WriteLine(faker.Create<int>().ToString());
